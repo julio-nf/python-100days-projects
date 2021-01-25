14fc9aa9691fd4f6996d2f9831cdd2ac84ca995d
Julio Novais <julio.novais@outlook.com>
2021-01-24 23:48:31 -0300

    feat: added day 1 project

---
+++ day1/band-name-generator.py
@@ -0,0 +1,9 @@
+# Day 1
+# -----
+# Project: Band Name Generator
+
+
+print('Welcome to the Band Name Generator')
+city = input('Whats name of the city you grew up in?\n')
+pet = input('Whats your pets name?\n')
+print('Your band name could be: ' + city + ' ' + pet)
